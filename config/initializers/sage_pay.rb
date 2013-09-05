763604380b5491831e9a4663b1c66fbe1ff130f2
Eugen <netoctone@gmail.com>
2013-09-05 15:11:42 +0300

    Change deploy params

--- config/initializers/sage_pay.rb
+++ config/initializers/sage_pay.rb
@@ -2,13 +2,13 @@
 
 # Our vendor name. Bad news, this one only works in the simulator, not
 # live. :-)
-SagePay::Server.default_registration_options[:vendor] = "rubaidh"
+SagePay::Server.default_registration_options[:vendor] = "netoctone"
 
 # Notification URL for pingbacks. Unfortunately, the URL helpers don't work in
 # here, or this would be +notifications_url+. On the plus side, it's submitted
 # as a POST, so we can pretend they meant to deliver a RESTful :create action.
 # :)
-SagePay::Server.default_registration_options[:notification_url] = "https://sage-pay-rails-example.heroku.com/notifications"
+SagePay::Server.default_registration_options[:notification_url] = "https://arcane-ridge-7513.herokuapp.com/notifications"
 
 # Set the mode globally. Normally, you'd probably want to have different
 # settings for different environments (eg :test in staging and :live in
